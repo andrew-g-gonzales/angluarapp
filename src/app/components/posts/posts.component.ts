f40237c8579555114f05e1f4c79b95585e41ef43
Andrew G. Gonzales <andrew.g.gonzales@avanade.com>
2020-09-28 14:54:49 -0400

    update

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -10,6 +10,12 @@ import { Post } from '../../models/Post';
 })
 export class PostsComponent implements OnInit {
   posts: Post[];
+  currentPost: Post = {
+    id: 0,
+    title: '',
+    body: ''
+  };
+  isEdit: boolean = false;
 
   constructor(private postService: PostService) { }
 
@@ -18,6 +24,16 @@ export class PostsComponent implements OnInit {
     this.postService.getPosts().subscribe(posts => {
       this.posts = posts;
     });
+  }
+
+  onNewPost(post: Post){
+    this.posts.unshift(post);
+  }
+
+  editPost(post: Post){
+
+    this.currentPost = post;
+    this.isEdit = true;
   }
 
 }
